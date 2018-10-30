298c52bb5fead790c1644f1771bfb4fbad056cf1
Shamail <zahis538@newschool.edu>
2018-10-30 10:47:25 -0400

    ok

--- js/02_logic.js
+++ js/02_logic.js
@@ -20,7 +20,7 @@ for ( var i = 0; i < 2000; i++) {
 	//add class "shape" to div
 	shape.classList.add('shape');
 
-		if (i % 2 == 0) {
+		if (i % 3 == 0) {
 			shape.classList.add('variation-1');
 		}
 
