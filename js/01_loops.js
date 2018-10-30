7b21086a9195ffe4b570dd670b52c5498345dc12
Shamail <zahis538@newschool.edu>
2018-10-30 10:19:12 -0400

    ok

--- js/01_loops.js
+++ js/01_loops.js
@@ -50,7 +50,7 @@ shapes.forEach(	function(shape, i) {
 
 var percentage = i / 200 * 100;
 
-	var color = hslColor( 0, percentage,50) ;
+	var color = hslColor( i + 120/ 4, percentage,50) ;
 	shape.style.background=color;
 });
 
